3f54a3f8ffec066edb19fb014f54067c1b44d0d9
wuzhubin <wzb3454920551@163.com>
2025-11-27 22:30:18 +0800

    后端修改

--- picture-backend/src/main/java/com/wzb/picturebackend/PictureBackendApplication.java
+++ picture-backend/src/main/java/com/wzb/picturebackend/PictureBackendApplication.java
@@ -1,9 +1,15 @@
 package com.wzb.picturebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@MapperScan("com.wzb.picturebackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class PictureBackendApplication {
 
     public static void main(String[] args) {

53a8a400950ed0b74a2f1833d3214a8f13005343
wuzhubin <wzb3454920551@163.com>
2025-11-27 21:53:26 +0800

    后端初始化

---
+++ picture-backend/src/main/java/com/wzb/picturebackend/PictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wzb.picturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureBackendApplication.class, args);
+    }
+
+}
